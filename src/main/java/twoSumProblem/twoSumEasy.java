6d6cf9a00755235cffbf9008ba26d82593bbaf66
aksrajvanshi <aksrajvanshi@gmail.com>
2021-05-27 09:39:43 -0700

    Added formatting checks and formatted files

--- src/main/java/twoSumProblem/twoSumEasy.java
+++ src/main/java/twoSumProblem/twoSumEasy.java
@@ -1,23 +1,25 @@
-package twoSumProblem; /**
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- *
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- *
- * You can return the answer in any order.
- * Example 1:
- *
- * Input: nums = [2,7,11,15], target = 9
- * Output: [0,1]
- * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
- * Example 2:
- *
- * Input: nums = [3,2,4], target = 6
- * Output: [1,2]
- * Example 3:
- *
- * Input: nums = [3,3], target = 6
- * Output: [0,1]
- */
+package twoSumProblem;
+
+/**
+* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+*
+* You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*
+* You can return the answer in any order.
+* Example 1:
+*
+* Input: nums = [2,7,11,15], target = 9
+* Output: [0,1]
+* Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+* Example 2:
+*
+* Input: nums = [3,2,4], target = 6
+* Output: [1,2]
+* Example 3:
+*
+* Input: nums = [3,3], target = 6
+* Output: [0,1]
+*/
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,14 +30,13 @@ public class twoSumEasy {
         Map<Integer, Integer> hmap = new HashMap<>();
         int[] res = new int[2];
 
-        for(int i=0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
 
-            if(hmap.containsKey(target - nums[i]) && i!= hmap.get(target - nums[i])){
+            if (hmap.containsKey(target - nums[i]) && i != hmap.get(target - nums[i])) {
                 res[0] = hmap.get(target - nums[i]);
                 res[1] = i;
                 return res;
-            }
-            else if(!hmap.containsKey(nums[i])){
+            } else if (!hmap.containsKey(nums[i])) {
                 hmap.put(nums[i], i);
             }
         }
--- src/test/java/twoSumTests/TestTwoSumClass.java
+++ src/test/java/twoSumTests/TestTwoSumClass.java
@@ -9,9 +9,9 @@ import twoSumProblem.twoSumEasy;
 public class TestTwoSumClass {
 
     @Test
-    public void testTwoSumEasy(){
+    public void testTwoSumEasy() {
 
-        int[] array1 = new int[]{2,7,11,15};
+        int[] array1 = new int[] { 2, 7, 11, 15 };
         int target1 = 9;
 
         twoSumEasy twoSumTestObject = new twoSumEasy();
@@ -19,23 +19,23 @@ public class TestTwoSumClass {
         int[] actualResult = twoSumTestObject.twoSum(array1, target1);
         Arrays.sort(actualResult);
 
-        Assert.assertArrayEquals(new int[]{0, 1}, actualResult);
+        Assert.assertArrayEquals(new int[] { 0, 1 }, actualResult);
 
-        int[] array2 = new int[]{3,2,4};
+        int[] array2 = new int[] { 3, 2, 4 };
         int target2 = 6;
 
         int[] actualResult2 = twoSumTestObject.twoSum(array2, target2);
         Arrays.sort(actualResult2);
 
-        Assert.assertArrayEquals(new int[]{1, 2}, actualResult2);
+        Assert.assertArrayEquals(new int[] { 1, 2 }, actualResult2);
 
-        int[] array3 = new int[]{3,3};
+        int[] array3 = new int[] { 3, 3 };
         int target3 = 6;
 
         int[] actualResult3 = twoSumTestObject.twoSum(array3, target3);
         Arrays.sort(actualResult3);
 
-        Assert.assertArrayEquals(new int[]{0, 1}, actualResult3);
+        Assert.assertArrayEquals(new int[] { 0, 1 }, actualResult3);
 
     }
 
