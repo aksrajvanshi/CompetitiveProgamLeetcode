2b4d617dccdb0cd42070717551feab7d7937b253
aksrajvanshi <aksrajvanshi@gmail.com>
2021-05-26 19:15:35 -0700

    Change folder structure for running tests

--- src/main/java/twoSumProblem/twoSumEasy.java
+++ src/main/java/twoSumProblem/twoSumEasy.java
@@ -1,4 +1,4 @@
-/**
+package twoSumProblem; /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -18,7 +18,6 @@
  * Input: nums = [3,3], target = 6
  * Output: [0,1]
  */
-package twoSum;
 
 import java.util.HashMap;
 import java.util.Map;
