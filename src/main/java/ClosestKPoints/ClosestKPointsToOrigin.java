4699a6fe1177f44ac9bfd8e039076b5c1e22959d
aksrajvanshi <aksrajvanshi@gmail.com>
2021-05-27 21:03:09 -0700

    Add Closest K points to Origin (Medium Level) question

---
+++ src/main/java/ClosestKPoints/ClosestKPointsToOrigin.java
@@ -0,0 +1,34 @@
+/**
+ * Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an
+ * integer k, return the k closest points to the origin (0, 0).
+ *
+ * <p>The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2
+ * + (y1 - y2)2).
+ *
+ * <p>You may return the answer in any order. The answer is guaranteed to be unique (except for the
+ * order that it is in).
+ */
+package ClosestKPoints;
+
+import java.util.PriorityQueue;
+
+public class ClosestKPointsToOrigin {
+
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        for (int[] point : points) {
+            pq.offer(point);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[][] res = new int[k][2];
+        while (k > 0) {
+            res[--k] = pq.poll();
+        }
+        return res;
+    }
+
+}
---
+++ src/test/java/ClosestKPoints/TestClosestKPointsToOrigin.java
@@ -0,0 +1,19 @@
+package ClosestKPoints;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClosestKPointsToOrigin {
+
+    @Test
+    public void testClosestKPointsToOrigin() {
+        ClosestKPointsToOrigin closestKPoints = new ClosestKPointsToOrigin();
+
+        Assert.assertArrayEquals(new int[][] { { 3, 3 }, { -2, 4 } },
+                closestKPoints.kClosest(new int[][] { { 3, 3 }, { 5, -1 }, { -2, 4 } }, 2));
+
+        Assert.assertArrayEquals(new int[][] { { -2, 2 } },
+                closestKPoints.kClosest(new int[][] { { 1, 3 }, { -2, 2 } }, 1));
+    }
+
+}
