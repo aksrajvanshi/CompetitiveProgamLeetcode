a75b9d43d6f3ccf7e3d0163276ddd53cf501c0c2
aksrajvanshi <aksrajvanshi@gmail.com>
2021-06-05 19:15:05 -0700

    Added problem 2. Add Two Numbers (Formatted)

--- src/main/java/linkedlists/ListNode.java
+++ src/main/java/linkedlists/ListNode.java
@@ -2,17 +2,18 @@ package linkedlists;
 
 public class ListNode {
 
-  int val;
-  ListNode next;
+    int val;
+    ListNode next;
 
-  ListNode() {}
+    ListNode() {
+    }
 
-  ListNode(int val) {
-    this.val = val;
-  }
+    ListNode(int val) {
+        this.val = val;
+    }
 
-  ListNode(int val, ListNode next) {
-    this.val = val;
-    this.next = next;
-  }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
