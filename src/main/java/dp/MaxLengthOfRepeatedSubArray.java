205d06ae17ee6d1814047c0acea1c67763cb425f
aksrajvanshi <aksrajvanshi@gmail.com>
2021-07-09 12:51:12 -0700

    Reformat files

--- src/main/java/dp/MaxLengthOfRepeatedSubArray.java
+++ src/main/java/dp/MaxLengthOfRepeatedSubArray.java
@@ -15,18 +15,17 @@ public class MaxLengthOfRepeatedSubArray {
 
     public int findLength(int[] nums1, int[] nums2) {
         int res = 0;
-        int[][] dp = new int[nums1.length+1][nums2.length+1];
+        int[][] dprog = new int[nums1.length + 1][nums2.length + 1];
 
-        for(int i=0; i < dp.length; i++){
-            for(int j=0; j < dp[0].length; j++){
+        for (int i = 0; i < dprog.length; i++) {
+            for (int j = 0; j < dprog[0].length; j++) {
 
-                if(i == 0 || j == 0){
-                    dp[i][j] = 0;
-                }
-                else if(nums1[i-1] == nums2[j-1]){
+                if (i == 0 || j == 0) {
+                    dprog[i][j] = 0;
+                } else if (nums1[i - 1] == nums2[j - 1]) {
 
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                    res = Math.max(res, dp[i][j]);
+                    dprog[i][j] = dprog[i - 1][j - 1] + 1;
+                    res = Math.max(res, dprog[i][j]);
                 }
             }
         }
--- src/test/java/dp/TestMaxLengthOfRepeatedSubArray.java
+++ src/test/java/dp/TestMaxLengthOfRepeatedSubArray.java
@@ -6,18 +6,14 @@ import org.junit.Test;
 public class TestMaxLengthOfRepeatedSubArray {
 
     @Test
-    public void testMaxLengthOfRepeatedSubArray(){
+    public void testMaxLengthOfRepeatedSubArray() {
         MaxLengthOfRepeatedSubArray maxLengthOfRepeatedSubArray = new MaxLengthOfRepeatedSubArray();
 
-        Assert.assertEquals(3, maxLengthOfRepeatedSubArray.findLength(
-                new int[]{1,2,3,2,1},
-                new int[]{3,2,1,4,7}
-        ));
+        Assert.assertEquals(3,
+                maxLengthOfRepeatedSubArray.findLength(new int[] { 1, 2, 3, 2, 1 }, new int[] { 3, 2, 1, 4, 7 }));
 
-        Assert.assertEquals(5, maxLengthOfRepeatedSubArray.findLength(
-                new int[]{0,0,0,0,0},
-                new int[]{0,0,0,0,0}
-        ));
+        Assert.assertEquals(5,
+                maxLengthOfRepeatedSubArray.findLength(new int[] { 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0 }));
     }
 
 }
